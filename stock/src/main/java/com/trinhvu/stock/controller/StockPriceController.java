d25bbb84aab123b6573eb1e57bee32f019485cc2
Kizey <81180330+trinhvu1711@users.noreply.github.com>
2024-10-03 22:03:50 +0700

    add feature generate price

--- stock/src/main/java/com/trinhvu/stock/controller/StockPriceController.java
+++ stock/src/main/java/com/trinhvu/stock/controller/StockPriceController.java
@@ -1,5 +1,6 @@
 package com.trinhvu.stock.controller;
 
+import com.trinhvu.stock.scheduler.StockPriceScheduler;
 import com.trinhvu.stock.service.StockPriceService;
 import com.trinhvu.stock.viewmodel.*;
 import lombok.RequiredArgsConstructor;
@@ -11,11 +12,25 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class StockPriceController {
     private final StockPriceService stockPriceService;
-
+    private final StockPriceScheduler stockPriceScheduler;
     @PostMapping
     public ResponseEntity<StocksGetVm> addStock(@RequestParam(value = "stockSymbol", required = true) String stockSymbol) {
         return ResponseEntity.ok(stockPriceService.updateStockPrice(stockSymbol));
     }
 
+    @GetMapping
+    public ResponseEntity<StocksPriceGetVm> getLastPrice(@RequestParam(value = "stockSymbol", required = true) String stockSymbol) {
+        return ResponseEntity.ok(stockPriceService.getLastPrice(stockSymbol));
+    }
+
+    @PostMapping("/start-task")
+    public void startTask(@RequestParam(value = "stockSymbol", required = true) String stockSymbol) {
+        stockPriceScheduler.startTask(stockSymbol);
+    }
+
+    @PostMapping("/stop-task")
+    public void stopTask() {
+        stockPriceScheduler.stopTask();
+    }
 
 }
