328cb2c5773b0491f99e2f484241859977397311
trinhvu1711 <81180330+trinhvu1711@users.noreply.github.com>
2025-01-06 22:12:10 +0700

    initial

---
+++ backoffice-bff/src/main/java/com/trinhvu/backofficebff/BackofficeBffApplication.java
@@ -0,0 +1,13 @@
+package com.trinhvu.backofficebff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackofficeBffApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackofficeBffApplication.class, args);
+    }
+
+}
